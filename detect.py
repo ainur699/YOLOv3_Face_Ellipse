fc7c17abf6b708e812265a0bd7c06454c34ddc17
ainur699 <zirgan.ainur@mail.ru>
2022-02-04 21:57:45 +0300

    edit

--- detect.py
+++ detect.py
@@ -1,14 +1,9 @@
-import time
 from absl import app, flags, logging
 from absl.flags import FLAGS
-import cv2
 import numpy as np
 import tensorflow as tf
-from models import (
-    YoloV3Face, YoloV3FaceTiny
-)
+from models import YoloV3FaceTiny
 import dataset
-import os
 import glob
 from tqdm import tqdm
 
@@ -23,27 +18,21 @@ flags.DEFINE_boolean('tiny', True, 'yolov3 or yolov3-tiny')
 flags.DEFINE_integer('size', 512, 'resize images to')
 
 
+# Detect ellipses in images from folder imgs. Output results in bbox folder near imgs.
 def main(_argv):
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    if len(physical_devices) > 0: tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-    if FLAGS.tiny:
-        yolo = YoloV3FaceTiny(FLAGS.size)
-    else:
-        yolo = YoloV3Face(FLAGS.size)
-
+    yolo = YoloV3FaceTiny(FLAGS.size)
     yolo.load_weights(FLAGS.weights).expect_partial()
 
-    filenames = glob.glob('D:/photolab_test/imgs/*')
+    filenames = glob.glob('D:/test_celeba/fear_synthetic/refs/target/imgs/*')
 
     for filename in tqdm(filenames):
         img, pad, max_shape = dataset.load_and_preprocess_image(filename)
         img = tf.expand_dims(img, 0)
-        
-        t1 = time.time()
+
         outputs = yolo(img)
-        t2 = time.time()
-        logging.info('time: {}'.format(t2 - t1))
 
         dataset.DrawOutputs(img[0], outputs[0], filename, (pad[0].numpy(), pad[1].numpy()), max_shape.numpy())
 
